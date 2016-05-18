2ec2809fd93b0ed3b489c6b36fb943bb7cd18897
diablo465 <cmzhang1318@hotmail.com>
2016-05-18 16:20:09 +1000

    commit the result for class

--- python/class_station.py
+++ python/class_station.py
@@ -86,7 +86,8 @@ class Station:
             self.lats_idx[n], min_value = min(enumerate(abs(a.latitude[n] -lats_ay)), key=operator.itemgetter(1))
             self.lons_idx[n], max_value = min(enumerate(abs(a.longitude[n]-lons_ay)), key=operator.itemgetter(1))
 
-    def extract_value_at_stations(self,evap_yearly_raw,rain_yearly_raw,lats,lons,day_count_yearly):
+    def extract_value_at_stations(self,evap_yearly_raw,rain_yearly_raw,lats,lons,day_count_yearly
+        ,evap_total_rate_mmday,rain_total_rate_mmday):
         # define a list of empty np array:
         #result = [np.zeros(5) for _ in xrange(3)]
         # result = [np.random.rand(5) for _ in xrange(3)]
@@ -96,9 +97,9 @@ class Station:
         self.evap_annual_mmday    = [np.zeros(self.station_no,dtype=float) for _ in np.arange(day_count_yearly.size)]   
         self.rain_annual_mmday    = [np.zeros(self.station_no,dtype=float) for _ in np.arange(day_count_yearly.size)]   
         self.net_evap_annual_mmday= [np.zeros(self.station_no,dtype=float) for _ in np.arange(day_count_yearly.size)]   
-        self.evap_avg_mmday       = np.zeros(self.station_no,dtype=float)
-        self.rain_avg_mmday       = np.zeros(self.station_no,dtype=float)
-        self.net_evap_avg_mmday   = np.zeros(self.station_no,dtype=float)
+        self.evap_total_avg_mmday       = np.zeros(self.station_no,dtype=float)
+        self.rain_total_avg_mmday       = np.zeros(self.station_no,dtype=float)
+        self.net_evap_total_avg_mmday   = np.zeros(self.station_no,dtype=float)
         #self.days_annual          = np.zeros(days_count_yearly.size,dtype=float)
         self.days_annual=day_count_yearly
 
@@ -107,10 +108,15 @@ class Station:
            self.evap_annual_mmday[n]     = -evap_yearly_raw[self.lats_idx,self.lons_idx,n]*1000/day_count_yearly[n]
            self.rain_annual_mmday[n]     = rain_yearly_raw[self.lats_idx,self.lons_idx,n]*1000/day_count_yearly[n]
            self.net_evap_annual_mmday[n] = self.evap_annual_mmday[n]-self.rain_annual_mmday[n]
+
+        self.evap_total_avg_mmday     = evap_total_rate_mmday[self.lats_idx,self.lons_idx]
+        self.rain_total_avg_mmday     = rain_total_rate_mmday[self.lats_idx,self.lons_idx]
+        self.net_evap_total_avg_mmday =  self.evap_total_avg_mmday-self.rain_total_avg_mmday 
         
 
     def interpolate_values_as_mtx(self,lats,lons):
         from scipy.interpolate import griddata
+        from scipy.interpolate import Rbf
         self.lats_mtx=lats
         self.lons_mtx=lons
         self.interp_evap_annual_mmday_mtx    = [np.zeros(lats.shape,dtype=float) for _ in np.arange(day_count_yearly.size)]
@@ -128,14 +134,37 @@ class Station:
             # 2. cubic tends to over estimate
             # 3. linear is good but not very accurate
             # 4. nearest is the most uggly one
-            self.interp_evap_annual_mmday_mtx[n] = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.evap_annual_mmday[n], (self.lats_mtx, self.lons_mtx), method='cubic(2-D)')
-            self.interp_rain_annual_mmday_mtx[n] = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.rain_annual_mmday[n], (self.lats_mtx, self.lons_mtx), method='cubic(2-D)')
+            self.interp_evap_annual_mmday_mtx[n] = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.evap_annual_mmday[n], (self.lats_mtx, self.lons_mtx), method='linear')
+            self.interp_rain_annual_mmday_mtx[n] = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.rain_annual_mmday[n], (self.lats_mtx, self.lons_mtx), method='linear')
             self.interp_net_evap_annual_mmday_mtx[n] = self.interp_evap_annual_mmday_mtx[n] -self.interp_rain_annual_mmday_mtx[n] 
+
+            ## now we are using rbf TO  2015-05-17
+            ##https://scipy.github.io/old-wiki/pages/Cookbook/RadialBasisFunctions.html
+            ## seems it is a bit slow
+            ## a better way of improving evaporation interploation is to interpolate the final net evap data
+            #aa=Rbf(self.latitude,self.longitude,self.evap_annual_mmday[n], epsilon=2)
+            #self.interp_evap_annual_mmday_mtx[n] = aa(self.lats_mtx, self.lons_mtx)
+            #bb=Rbf(self.latitude,self.longitude,self.rain_annual_mmday[n], epsilon=2)
+            #self.interp_rain_annual_mmday_mtx[n] = bb(self.lats_mtx, self.lons_mtx)
+
+
+
+
         # it is funny that python doesn't have a function to average a list
         # http://stackoverflow.com/questions/9039961/finding-the-average-of-a-list
         self.interp_evap_total_avg_mmday_mtx = sum(self.interp_evap_annual_mmday_mtx)/float(len(self.interp_evap_annual_mmday_mtx))
         self.interp_rain_total_avg_mmday_mtx = sum(self.interp_rain_annual_mmday_mtx)/float(len(self.interp_rain_annual_mmday_mtx))
         self.interp_net_evap_total_avg_mmday_mtx=self.interp_evap_total_avg_mmday_mtx-self.interp_rain_total_avg_mmday_mtx
+
+
+        # the below result only do one interpolation based on the averaged last image
+        self.one_interp_evap_total_avg_mmday_mtx = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.evap_total_avg_mmday, (self.lats_mtx, self.lons_mtx), method='linear')
+        self.one_interp_rain_total_avg_mmday_mtx = griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.rain_total_avg_mmday, (self.lats_mtx, self.lons_mtx), method='linear')
+        self.one_interp_net_evap_total_avg_mmday_mtx=griddata(np.hstack([self.latitude[np.newaxis, :].T,self.longitude[np.newaxis, :].T]), self.evap_total_avg_mmday-self.rain_total_avg_mmday, (self.lats_mtx, self.lons_mtx), method='linear')
+
+
+
+
         
     def plot_interpolated_annual_avg_result(self):
         for n in np.arange(len(self.days_annual)):
@@ -196,22 +225,23 @@ class Station:
             im=ax1.contourf(self.lons_mtx,self.lats_mtx,self.interp_evap_total_avg_mmday_mtx) #,levels=v)
             ax1.plot(self.longitude,self.latitude,'r+')
             ax1.set_ylabel('latitude')
-            ax1.set_title('Interpolated average Evaporation (mm/day) in '+str(1979+n))
+            ax1.set_title('Interpolated average Evaporation (mm/day) over 32 years' )
             fig.colorbar(im,ax=ax1) #,ticks=v)
             #print 'the max evap is: '+str(np.max(evap_total))+'m, minimum evap is: '+ str(np.min(evap_total))+' m'
             #
             #
-            im=ax2.contourf(self.lons_mtx,self.lats_mtx,self.interp_rain_annual_mmday_mtx[n]) #,levels=v)
+            im=ax2.contourf(self.lons_mtx,self.lats_mtx,self.interp_rain_total_avg_mmday_mtx) #,levels=v)
             ax2.plot(self.longitude,self.latitude,'r+')
             ax2.set_ylabel('latitude')
-            ax2.set_title('Interpolated average Precipitation (mm/day) in '+str(1979+n))
+            ax2.set_title('Interpolated average Precipitation (mm/day) over 32 years')
             fig.colorbar(im,ax=ax2)
             #print 'the max rain is: '+str(np.max(rain_total))+'m, minimum evap is: '+ str(np.min(rain_total))+' m'
             #
             ##print 'Processing time'+ unicode(data_date_rain)+ ', for precipitation finished'+str(lats.shape)
-            im=ax3.contourf(self.lons_mtx,self.lats_mtx,self.interp_rain_total_avg_mmday_mtx) #,levels=v)
+            im=ax3.contourf(self.lons_mtx,self.lats_mtx,
+                self.interp_evap_total_avg_mmday_mtx-self.interp_rain_total_avg_mmday_mtx)
             ax3.plot(self.longitude,self.latitude,'r+')
-            ax3.set_title('Interploated average Evaporation-precipitation (mm/day) in ' +str(1979+n))
+            ax3.set_title('Interploated average Evaporation-precipitation (mm/day) over 32 years')
             ax3.set_ylabel('latitude')
             ax3.set_xlabel('longitude')
             #ax3.set_title('Precipitation + evaporation (m) at UTC time: '
@@ -222,7 +252,6 @@ class Station:
             #
             print 'interpolated_total_avg_evap_rain.png'
             #csv_name_total='interpolated_annual_average_net_evaporation_mmday_in_year_'+str(1979+n)+'.csv'
-            
             #np.savetxt(csv_name_rain, evap_annual_avg_mmday, delimiter=",")
             #np.savetxt(csv_name_evap, rain_annual_avg_mmday, delimiter=",")
             #np.savetxt(csv_name_total, evap_annual_avg_mmday-rain_annual_avg_mmday, delimiter=",")
@@ -230,6 +259,47 @@ class Station:
             fig.savefig(fig_name,format='png')
             plt.close(fig)
 
+
+
+            # the below is the image that only does the one off image
+            fig=plt.figure(figsize=(20,25))
+            ax1 = fig.add_subplot(311)
+            ax2 = fig.add_subplot(312)
+            ax3 = fig.add_subplot(313)
+            #v = np.linspace(-0.02,0.02, 21, endpoint=True)
+            
+            im=ax1.contourf(self.lons_mtx,self.lats_mtx,self.one_interp_evap_total_avg_mmday_mtx) #,levels=v)
+            ax1.plot(self.longitude,self.latitude,'r+')
+            ax1.set_ylabel('latitude')
+            ax1.set_title('Interpolated average Evaporation (mm/day) over 32 years' )
+            fig.colorbar(im,ax=ax1) #,ticks=v)
+            #print 'the max evap is: '+str(np.max(evap_total))+'m, minimum evap is: '+ str(np.min(evap_total))+' m'
+            #
+            #
+            im=ax2.contourf(self.lons_mtx,self.lats_mtx,self.one_interp_rain_total_avg_mmday_mtx) #,levels=v)
+            ax2.plot(self.longitude,self.latitude,'r+')
+            ax2.set_ylabel('latitude')
+            ax2.set_title('Interpolated average Precipitation (mm/day) over 32 years')
+            fig.colorbar(im,ax=ax2)
+            im=ax3.contourf(self.lons_mtx,self.lats_mtx,
+                self.one_interp_evap_total_avg_mmday_mtx-self.one_interp_rain_total_avg_mmday_mtx)
+            ax3.plot(self.longitude,self.latitude,'r+')
+            ax3.set_title('Interploated average Evaporation-precipitation (mm/day) over 32 years')
+            ax3.set_ylabel('latitude')
+            ax3.set_xlabel('longitude')
+            fig.colorbar(im,ax=ax3)
+            #fig.show()
+            #
+            print 'one_interpolated_total_avg_evap_rain.png'
+            csv_name_total_evap='one_interpolated_total_avg_evap_mmday_over_32_years.csv'
+            csv_name_total_rain='one_interpolated_total_avg_rain_mmday_over_32_years.csv'
+            csv_name_total_net_evap='one_interpolated_total_avg_net_evap_mmday_over_32_years.csv'
+            np.savetxt(csv_name_total_evap, self.one_interp_evap_total_avg_mmday_mtx, delimiter=",")
+            np.savetxt(csv_name_total_rain, self.one_interp_rain_total_avg_mmday_mtx, delimiter=",")
+            np.savetxt(csv_name_total_net_evap, self.one_interp_evap_total_avg_mmday_mtx-self.one_interp_rain_total_avg_mmday_mtx, delimiter=",")
+            fig_name='one_interpolated_average_evap_rain_in_total.png'
+            fig.savefig(fig_name,format='png')
+            plt.close(fig)
         
     def area(self):
         return self.x * self.y
@@ -247,7 +317,52 @@ class Station:
         return self.row_count
 
 
+    def output_csv_for_station(self):
+        #http://stackoverflow.com/questions/2721521/fastest-way-to-generate-delimited-string-from-1d-numpy-array
+        year_str=np.char.mod('%i', np.arange(1979,2011))
+        year_str_comma_sep = ",".join(year_str)
+        header='station_name,'+year_str_comma_sep+',total_average'
+        
+        # http://stackoverflow.com/questions/14134237/writing-data-from-a-python-list-to-csv-row-wise
+        #writer=csv.writer(open('aaa.csv','wb',0))
+        #header=['type','id','numberOfUpdates','isPingEnabled','lastUpdated']
+        #length_list=len(header)
+        #for word in header:
+        #    writer.writerow([word])
+        fid= open('evap_per_station.csv','wb',0)
+        fid.write(header)
+        fid.write('\n')
+        for n in np.arange(a.station_no):
+            line=np.char.mod('%e', np.append( np.array([self.evap_annual_mmday[i][n] for i in np.arange(len( self.evap_annual_mmday))]) , self.evap_total_avg_mmday[n]))
+            line= ",".join(line)
+            line=self.station_name[n]+','+line
+            fid.write(line)
+            fid.write('\n')
+        fid.close()
 
+
+
+        fid= open('rain_per_station.csv','wb',0)
+        fid.write(header)
+        fid.write('\n')
+        for n in np.arange(a.station_no):
+            line=np.char.mod('%e', np.append( np.array([self.rain_annual_mmday[i][n] for i in np.arange(len( self.rain_annual_mmday))]) , self.rain_total_avg_mmday[n]))
+            line= ",".join(line)
+            line=self.station_name[n]+','+line
+            fid.write(line)
+            fid.write('\n')
+        fid.close()
+    
+        fid= open('net_evap_per_station.csv','wb',0)
+        fid.write(header)
+        fid.write('\n')
+        for n in np.arange(a.station_no):
+            line=np.char.mod('%e', np.append( np.array([self.net_evap_annual_mmday[i][n] for i in np.arange(len( self.net_evap_annual_mmday))]) , self.net_evap_total_avg_mmday[n]))
+            line= ",".join(line)
+            line=self.station_name[n]+','+line
+            fid.write(line)
+            fid.write('\n')
+        fid.close()
 #import csv
 #with open('site_address.csv', 'rb') as f:
 #    reader = csv.reader(f)
--- python/run_script_for_interplolation.py
+++ python/run_script_for_interplolation.py
@@ -5,10 +5,12 @@ execfile(os.path.expanduser('~')+"/Dropbox/scripts/github/ecmwf_reader/python/tr
 execfile(os.path.expanduser('~')+"/Dropbox/scripts/github/ecmwf_reader/python/class_station.py")
 a=Station('asdf')
 a.find_lats_lons_idx_from_mtx(lats,lons) 
-a.extract_value_at_stations(evap_yearly_raw,rain_yearly_raw,lats,lons,day_count_yearly)
+a.extract_value_at_stations(evap_yearly_raw,rain_yearly_raw,lats,lons,day_count_yearly
+        ,evap_total_rate_mmday,rain_total_rate_mmday)
 a.interpolate_values_as_mtx(lats,lons)
-a.plot_interpolated_annual_avg_result()
+#a.plot_interpolated_annual_avg_result()
 a.plot_interpolated_total_avg_result()
+a.output_csv_for_station()
 
 
 
